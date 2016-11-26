e881ad08e918f9cde6bc9baf918575dab614904f
jebework <rappoluwf@hotmail.com>
2016-11-27 00:53:29 +0800

    added ttjj some pages

--- test.py
+++ test.py
@@ -6,6 +6,15 @@ import urllib2
 import re
 # import urllib.request
 
+#http://fund.eastmoney.com/f10/jjjz_000697.html
+#http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code=000697&page=1&per=20&sdate=&edate=&rt=0.2289961661162161
+#http://fund.eastmoney.com/js/fundcode_search.js
+
+#http://fund.eastmoney.com/f10/FundArchivesDatas.aspx?type=jjcc&code=000697&topline=10&year=&month=&rt=0.8884351801845889
+#http://fund.eastmoney.com/f10/F10DataApi.aspx?type=hypz&code=000697&year=2016&rt=0.21980476212178313
+#http://fund.eastmoney.com/f10/FundArchivesDatas.aspx?type=hypzsy&code=000697&rt=0.5281546500642944
+#http://fund.eastmoney.com/f10/FundArchivesDatas.aspx?type=gmbd&mode=0&code=000697&rt=0.09567990767947365
+#http://fund.eastmoney.com/f10/FundArchivesDatas.aspx?type=cwzb&code=000697&showtype=1&year=&rt=0.03965805350296048
 
 
 class Spider:
@@ -34,4 +43,4 @@ class Spider:
 
 
 spider = Spider()
-spider.getContents(1)
+spider.getContents(2)
