fa0bcd9003c8dbb0a5c013356c78f328a74e2821
jebework <rappoluwf@hotmail.com>
2018-05-20 15:16:10 +0800

    save to csv, and make accu to dataframe.

--- test.py
+++ test.py
@@ -9,6 +9,12 @@ import time
 import numpy as np
 import pandas as pd
 import csv
+import matplotlib.pyplot as plt
+import sys
+import datetime
+
+
+
 
 #import urllib.request
 #http://fund.eastmoney.com/f10/jjjz_000697.html
@@ -49,7 +55,14 @@ class Spider:
 #spider = Spider()
 #spider.getContents(2)
 
+
+
+
+
 class TtjjSpider:
+    
+    
+    
     def __init__(self):
         self.siteURL = 'http://fund.eastmoney.com/f10/'
 
@@ -64,10 +77,57 @@ class TtjjSpider:
 
         return response.read()
 
+    def getFirstPage(self, code, per):
+        url = 'http://fund.eastmoney.com'
+        #print url
+
+        #request = urllib.request.Request(url)
+        #response = urllib.request.urlopen(request)
+        request = urllib2.Request(url)
+        response = urllib2.urlopen(request)
+
+        return response.read()
+
+    def getFirstPageContents(self, code):
+        page = self.getFirstPage(code, 1)
+        #print page
+        #pattern = re.compile('var apidata={ content:"<table class=.*?>.*?</table>",records:(.*?),pages:.*?,curpage:.*?};',re.S)
+        #pattern = re.compile('<div class="wrapper fund_margin10">.*?class="fname">(.*?)</a>.*?fc=(.*?)".*?<a .*?class="fname">(.*?)</a>.*?fc=(.*?)".*?class="fname">(.*?)</a>.*?fc=(.*?)".*?<div class="dataShow-item dataShow-border">', re.S)
+        #pattern = re.compile('<!--published at (.*?) (.*?)-->.*?<div class="wrapper fund_margin10">.*?class="fname">(.*?)</a>.*?fc=(.*?)".*?<a .*?class="fname">(.*?)</a>.*?fc=(.*?)".*?class="fname">(.*?)</a>.*?fc=(.*?)".*?<div class="dataShow-item dataShow-border">', re.S)
+
+        datetimepattern = '<!--published at (.*?) (.*?)-->'
+        highpattern = '.*?<div class="wrapper fund_margin10">.*?class="fname">(.*?)</a>.*?fc=(.*?)".*?<a .*?class="fname">(.*?)</a>.*?fc=(.*?)".*?class="fname">(.*?)</a>.*?fc=(.*?)"'
+        goodpattern = '.*?<div class="dataShow-item dataShow-border">.*?class="fname">(.*?)</a>.*?fc=(.*?)".*?<a .*?class="fname">(.*?)</a>.*?fc=(.*?)".*?class="fname">(.*?)</a>.*?fc=(.*?)"'
+        #endpattern = '.*?<div class="dataShow-item dataShow-border">'
+        hotpattern='.*?<div class="dataShow-item dataShow-border">.*?class="fname">(.*?)</a>.*?class="fname">(.*?)</a>.*?class="fname">(.*?)</a>.*?class="fname">(.*?)</a>.*?class="fname">(.*?)</a>'
+        endpattern = '.*?<div class="dataShow-item">'
+        pattern = re.compile(datetimepattern+highpattern+goodpattern+hotpattern+endpattern, re.S)
+
+
+        items = re.findall(pattern, page)
+        #print items
+
+        with open('ttjj.csv', 'a+') as f:
+            writer = csv.writer(f)
+            #write.writerows(['date' 'Adj' 'Adj'])
+
+            #mydate = time.strftime('%Y-%m-%d')
+            #writer.writerows(datetime)
+            #items.append(''.join(mydate))
+            writer.writerows(items)
+
+        #print items
+        print 'Done!'
+        f.close()
+
+#python -m py_compile test.py
+#mv test.pyc test0520a.pyc
+#python test0520a.pyc
+
+
     def getContents(self, code):
         page = self.getPage(code, 1)
         #print page
-
         pattern = re.compile('var apidata={ content:"<table class=.*?>.*?</table>",records:(.*?),pages:.*?,curpage:.*?};',re.S)
         recordsStr = re.findall(pattern, page)
         records = int(('').join(recordsStr))
@@ -77,8 +137,12 @@ class TtjjSpider:
         #print page
         pattern = re.compile(
             '<tr><td>(.*?)</td><td class=.*?>(.*?)</td><td class=.*?>(.*?)</td>.*?</tr>',re.S)
+        #pattern=re.compile(
+        #    '<tr><td>(.*?)</td><td class=.*?>(.*?)</td><td class=.*?>.*?</td><td class=.*?>.*?%</td>.*?</tr>',re.S)
 
         items = re.findall(pattern, page)
+
+        items.reverse()
 
         print len(items)
 
@@ -86,13 +150,13 @@ class TtjjSpider:
             #print(time.strptime(item[0], "%Y-%m-%d"), string.atof(item[1]), string.atof(item[2]))
             #print(item[0], item[1], item[2])
 
-        print items[1]
+        print items[0]
 
 
         with open('stocks.csv', 'w') as f:
             writer = csv.writer(f)
+            #write.writerows(['date' 'Adj' 'Adj'])
             writer.writerows(items)
-
 
         date = list()
         with open('stocks.csv', 'r') as f:
@@ -103,31 +167,59 @@ class TtjjSpider:
                 date.append(row[0])
         #print date
 
-        daily = list()
+        price = list()
         with open('stocks.csv', 'r') as f:
             reader = csv.reader(f)
             for row in reader:
                 #if reader.line_num == 1:
                 #    continue
-                daily.append(string.atof(row[1]))
+                price.append(string.atof(row[1]))
         #print daily
 
-
-        accu = list()
+        rtn = list()
         with open('stocks.csv', 'r') as f:
             reader = csv.reader(f)
             for row in reader:
                 #if reader.line_num == 1:
                 #    continue
-                accu.append(string.atof(row[2]))
+                rtn.append(string.atof(row[2]))
         #print accu
 
-        #print daily+accu
+        df = pd.DataFrame(price , index = date, columns=list('A')) #['price', 'rtn'])
 
-        df = pd.DataFrame(accu, index = date, columns=list('A')) #['Daily', 'Accu'])
+
 
         print df.describe()
 
+        plt.show(df.plot())
+
+#df=pd.read_csv("stocks.csv")
+#print df.describe()
+#plt.show(df.plot())
 
 spider = TtjjSpider()
-spider.getContents("000697")
+#spider.getContents("001878")  #001878
+spider.getFirstPageContents("001878")  #001878
+
+
+#>>> import test
+#>>> spider=test.TtjjSpider()
+#>>> spider.getContents("001878")
+
+
+#sys.exit()
+
+#f = pd.read_csv("./stocks.csv")
+#f.values
+#f.ix[:,1]
+#f.ix[:,1].valuesc
+#p=f.ix[:,1]
+#q=p.shift(-1)
+#(q-p)/p
+#t=f.ix[:,0]
+#r=(q-p)/p
+#df = pd.DataFrame(r , index = t, columns=list('return'))
+
+
+
+
